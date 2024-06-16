2a1cf2f07e7c053d1e23e156129f56fdc9ff06b0
mustafa.bilgic41 <mustafa.bilgic41@yandex.com>
2024-06-16 03:16:16 +0300

    Log4j eklendi

--- src/main/java/com/srs/backend/service/UserService.java
+++ src/main/java/com/srs/backend/service/UserService.java
@@ -43,6 +43,7 @@ public class UserService {
             userRepository.save(user);
             return true;
         } else {
+            log.info("there is no user !!!!!!");
             return false;
         }
     }
